c6c0163f5777d8f26586123a052eee099295a72a
otanadzetsotne <otanadzetsotne@yahoo.com>
2021-12-01 12:49:45 +0400

    TMP commit

--- tmp.py
+++ tmp.py
@@ -5,8 +5,9 @@ from api_key import API_KEY
 openai.api_key = API_KEY
 response = openai.Completion.create(
     engine='davinci',
-    prompt='Ehllo!',
-    temprature=.3,
+    prompt='What is love?',
+    max_tokens=56,
+    top_p=.01,
 )
 
 print(response)
